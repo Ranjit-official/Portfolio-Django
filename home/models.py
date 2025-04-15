9ec4fe380b32a5805a9bcb6ebecab183772ebae4
clever-king-402 <122508802+clever-king-402@users.noreply.github.com>
2025-04-15 22:42:39 +0545

    models cretifi exper tech

--- home/models.py
+++ home/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Certification(models.Model):
+    name = models.CharField(max_length=200,blank=False)
+    slug = models.CharField(unique=True,max_length=100,blank=False)
+    image = models.ImageField(upload_to="media/certification")
+
+    def __str__(self):
+        return self.name
+    
+class Experience(models.Model):
+    name = models.CharField(max_length=200,blank=False)
+    post = models.CharField(max_length=100,blank=False)
+    startDate = models.CharField(max_length=50)
+    endDate = models.CharField(max_length=50)
+    image =models.ImageField(upload_to='media/experience')
+    slug = models.CharField(max_length=200,unique=True,blank=False)
+
+    def __str__(self):
+        return f"{self.name} + {self.post}"
+
+class TechnicalSkills(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.CharField(max_length=100,unique=True,blank=False)
+
+    def __str__(self):
+        return self.name
+    
